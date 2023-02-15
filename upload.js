b84fc7e9626c9353cc919df786d0be05e0c29e38
WameJoseph <101824766+WameJoseph@users.noreply.github.com>
2023-02-15 12:38:36 +0100

    Add files via upload

---
+++ upload.js
@@ -0,0 +1,12 @@
+function closemode(){
+    document.getElementById('modal').style.display = 'none';
+    document.body.style.overflow = "scroll"
+
+}
+
+function openmode(){
+    document.getElementById('modal').style.display = 'flex';
+    document.body.style.overflow = "hidden"
+
+
+}
