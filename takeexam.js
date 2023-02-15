0218d50aea19726ce066ec2046ce9238282322bd
WameJoseph <101824766+WameJoseph@users.noreply.github.com>
2023-02-15 18:08:21 +0100

    Add files via upload

---
+++ takeexam.js
@@ -0,0 +1,7 @@
+function closeModal(){
+    document.getElementById('discussionnmodal').style.display = 'none';
+}
+
+function openmodal(){
+    document.getElementById('discussionnmodal').style.display = 'flex';
+}
--- upload.js
+++ upload.js
@@ -1,12 +1,12 @@
 function closemode(){
     document.getElementById('modal').style.display = 'none';
-    document.body.style.overflow = "scroll"
+    // document.body.style.overflow = "scroll"
 
 }
 
 function openmode(){
     document.getElementById('modal').style.display = 'flex';
-    document.body.style.overflow = "hidden"
+    // document.body.style.overflow = "hidden"
 
 
 }
